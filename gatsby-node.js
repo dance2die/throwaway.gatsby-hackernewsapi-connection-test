4fc4007eb91de1195fadfb15f4718717390dc2b0
Sung Kim <sungmkim80@gmail.com>
2018-05-22 23:24:32 -0400

    Created a HN plugin

--- gatsby-node.js
+++ gatsby-node.js
@@ -3,5 +3,38 @@
  *
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
+const axios = require('axios')
+const crypto = require('crypto')
 
- // You can delete this file if you're not using it
+exports.sourceNodes = async ({ boundActionCreators }) => {
+  const { createNode } = boundActionCreators
+
+  const res = await axios.get(
+    `https://hacker-news.firebaseio.com/v0/topstories.json`
+  )
+
+  console.log(`res`, res)
+
+  res.data.map((storyId, index) => {
+    const storyIdNode = {
+      id: `${index}`,
+      parent: null,
+      internal: {
+        type: `StoryIds`,
+      },
+      children: [],
+      storyId: storyId,
+    }
+
+    // Get content digest of node. (Required field)
+    const contentDigest = crypto
+      .createHash(`md5`)
+      .update(JSON.stringify(storyIdNode))
+      .digest(`hex`)
+    // add it to userNode
+    storyIdNode.internal.contentDigest = contentDigest
+
+    // Create node with the gatsby createNode() API
+    createNode(storyIdNode)
+  })
+}
